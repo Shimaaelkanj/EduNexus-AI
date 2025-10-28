a02b0ae65267d0da1b9825db8a8df4d7f07ab4f3
mzeineddine <mohammad.zeineddine50@gmail.com>
2025-10-28 21:25:17 +0200

    Changing file name

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     # path("test-mongo/", views.test_mongo, name="test_mongo"),
-#     # path("users/", views.users_list, name="users_list"),
-#     # path("list-databases/", views.list_databases, name="list_databases"),
-#     # path("parse/", views.parse_file_api, name="parse_file"),
-#     # path("summarize/", views.summarize_text_api, name="summarize_text"),
-#     # path("outline/", views.generate_outline_api, name="generate_outline"),
-#     path('upload/', views.upload_and_extract, name='upload_and_extract'),
-#     path('summarize/', views.summarize, name='summarize'),
-#     path('lessons/<str:lesson_id>/', views.lesson_detail, name='lesson_detail'),
-# ]
-
 from django.urls import path
 from core.views import (
     summarize_title,
